299a12abc8803dd65ecf1e7f0c29026301641274
stephenyau <softwareentrepreneer@gmail.com>
2024-04-07 16:32:43 +0800

    restructure

--- pfolio/main.py
+++ pfolio/main.py
@@ -1,22 +1,6 @@
-from pfolio.universe import Universe
-from pfolio.portfolio import Portfolio
-from pfolio.preference import Preference
+import pfolio as po
+import pfolio.analytics as pa
 
 
 if __name__ == '__main__':
-    # TODO:
-    positions = {}
-    balances = {}
-    universe = Universe()
-    preference = Preference()
-    portfolio = Portfolio(
-        universe,
-        positions,
-        balances,
-        preference=preference,
-        # TODO:
-        # config={
-        #     'min_weight': 0.01,
-        #     'max_weight': 0.15,
-        # }
-    )
+    pass
